57ece0e1c4dc622a53735e105f5cd09a544769c4
Ben Schwarz <ben.schwarz@gmail.com>
2012-12-07 17:23:44 +1100

    Added ability to remove image by tapping x. - Converted tabs to spaces.

--- tumblr.js
+++ tumblr.js
@@ -8,6 +8,7 @@
     nameField: document.querySelector( 'input' ),
     countField: document.querySelector( '.count' ),
     sourceField: document.querySelector( '.source' ),
+    imageHolder: document.querySelector( '#image-holder' ),
 
     url: function ( offset ) {
       return 'http://api.tumblr.com/v2/blog/' + Tumblr.name + '.tumblr.com/posts?api_key=PyezS3Q4Smivb24d9SzZGYSuhMNPQUhMsVetMC9ksuGPkK1BTt&offset=' + Tumblr.offset + '&callback=Tumblr.response';
@@ -23,7 +24,16 @@
       Tumblr.request();
       Tumblr.changeImage();
 
+      Tumblr.initKeyboard();
+
       setInterval( Tumblr.changeImage, Tumblr.displayTime );
+    },
+
+    initKeyboard: function () {
+      key( 'x', function () { 
+        Tumblr.purgeCurrentImage();
+        Tumblr.changeImage();
+      } );
     },
 
     updateDisplay: function () {
@@ -41,14 +51,15 @@
     response: function ( json ) {
       if ( json.response.posts.length > 0 ) {
         var gifs = Tumblr.getGifs( JSON.stringify( json ) );
-
+        
         Tumblr.posts = Tumblr.posts.concat( gifs );
 
         Tumblr.storage.set();
+        Tumblr.updateDisplay();
 
         setTimeout( function () {
           Tumblr.increaseOffset();
-          Tumblr.request();
+          Tumblr.request(); 
         }, 5000 );
       }
     },
@@ -65,12 +76,19 @@
 
     changeImage: function () {
       if ( Tumblr.posts.length ) {
-        var i = Math.floor( Tumblr.posts.length * Math.random() ),
-            url = Tumblr.posts[i];
+        var i = Math.floor( Tumblr.posts.length * Math.random() );
+        
+        Tumblr.currentImage = Tumblr.posts[i];
+        Tumblr.imageHolder.style.backgroundImage = 'url(' + Tumblr.currentImage + ')'; 
+      }
+    },
 
-        document.querySelector( '#image-holder' ).style.backgroundImage = 'url(' + url + ')';
+    purgeCurrentImage: function () {
+      var imageIndex = Tumblr.posts.indexOf( Tumblr.currentImage );
 
-        return url;
+      if ( imageIndex >= 0 ) {
+        Tumblr.posts.splice( imageIndex, 1 );
+        Tumblr.storage.set();
       }
     },
 
@@ -83,11 +101,11 @@
         var store = JSON.parse( localStorage.getItem( Tumblr.name ) ) || { posts: [] };
         store.offset = Tumblr.offset;
 
-        for ( var i = 0; i < Tumblr.posts.length; i++ ) {
+        for ( var i = 0; i < Tumblr.posts.length; i++ ) { 
           var post = Tumblr.posts[i];
 
           if ( store.posts.indexOf( post ) < 0 ) {
-            store.posts.push( post );
+            store.posts.push( post ); 
           }
         }
 
@@ -106,6 +124,7 @@
   };
 
   window.Tumblr = Tumblr;
+
   Tumblr.init(Util.getParameterByName( 't' ) || 'classics');
 
 }( window ) );

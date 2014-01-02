eba2eb56220a1f1eacd550b2bd5cb5c132dce480
Glen Maddern <glenmaddern@gmail.com>
2014-01-02 19:27:45 +1100

    got a simple JS concat system in there now

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat');
 
 // SASS
 gulp.task('sass', function () {
@@ -17,9 +18,26 @@ gulp.task('copy', function () {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('js', function () {
+  // Vendor the JS by symlinking into js/vendor/*.js
+  gulp.src('src/js/vendor/*.js')
+    .pipe(concat("vendor.js"))
+    .pipe(gulp.dest('dist/js'))
+
+  // Concat all the non-vendored JS into djgif.js
+  gulp.src(['src/js/**/*.js', '!src/js/vendor/**'])
+    .pipe(concat("djgif.js"))
+    .pipe(gulp.dest('dist/js'))
+});
+
 gulp.task('default', function () {
   // Compile everything to start with
-  gulp.run('sass', 'copy');
+  gulp.run('sass', 'copy', 'js');
+
+  // Watch JS
+  gulp.watch('src/js/**', function (e) {
+    gulp.run('js');
+  });
 
   // Watch SASS
   gulp.watch('src/sass/*.scss', function (e) {

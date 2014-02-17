3a8ef8938f8dd4f023792ec092bba01da4209909
Glen Maddern <glenmaddern@gmail.com>
2014-02-18 08:36:43 +1100

    wip from ages ago

--- src/js/controllers/gif_playback_ctrl.js
+++ src/js/controllers/gif_playback_ctrl.js
@@ -5,7 +5,6 @@
 //    GifSequence.addGif("http://i.imgur.com/Rhm49Ev.gif")
 
     GifSequence.$watch('currentGif', function (newVal) {
-      console.log("WATCHING " + newVal)
       if (newVal) $scope.frames = newVal.frames;
     })
   });

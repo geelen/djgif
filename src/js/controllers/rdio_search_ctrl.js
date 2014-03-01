e0b8ad51f65858519da9ff9aec8e3148b8b8c17b
Glen Maddern <glenmaddern@gmail.com>
2014-03-01 13:20:08 +1100

    first level of searching the API

--- src/js/controllers/rdio_search_ctrl.js
+++ src/js/controllers/rdio_search_ctrl.js
@@ -1,8 +1,15 @@
 ;(function (app) {
   'use strict';
 
-  app.controller('RdioSearchCtrl', function ($scope) {
+  app.controller('RdioSearchCtrl', function ($scope, $http) {
+    $scope.search = {};
 
+    $scope.searchGo = function () {
+      $http.get("http://localhost:5000/search", {params: {term: $scope.search.term}})
+        .then(function (response) {
+          $scope.results = response.data.result.results;
+        })
+    }
   })
 
 })(angular.module('djgif'));

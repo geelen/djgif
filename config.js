734513da901beaba8ed4347f09faa962fa487545
Glen Maddern <glenmaddern@gmail.com>
2014-12-30 20:28:53 +1100

    YEAAAAHHHHHHHHH GETTING X-GIF IN THERE AS A JS-ONLY CUSTOM ELEMENT YEAAAHHAHAHAHAH;

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ System.config({
   "map": {
     "fkit": "npm:fkit@0.16.2",
     "jsonp": "npm:jsonp@0.0.4",
+    "x-gif": "github:geelen/x-gif@0.1.1",
     "github:jspm/nodelibs@0.0.8": {
       "Base64": "npm:Base64@0.2.1",
       "base64-js": "npm:base64-js@0.0.7",
